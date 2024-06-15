437f223c44dcc780885a966acacd2e9f4dfdb02d
YRYYYYY <92637997+YRYYYYY@users.noreply.github.com>
2024-06-15 19:08:17 +0300

    Update process_cards.py

--- process_cards.py
+++ process_cards.py
@@ -132,7 +132,7 @@ def process_cards(cards):
     'mailpoet_woocommerce_checkout_optin_present': '1',
     'terms': 'on',
     'terms-field': '1',
-    'woocommerce-process-checkout-nonce': 'd0ddf04f7e',
+    'woocommerce-process-checkout-nonce': 'b82e91ddd2',
     '_wp_http_referer': '/?wc-ajax=update_order_review'
     
     }
